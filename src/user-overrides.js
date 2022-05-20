3e38d0f880c79c5f59e085a104916ee824fa902d
Ebert Matthee <ebert@ecmatthee.com>
2022-05-20 12:15:49 +0200

    Disabled urlbar topsite suggestions

--- src/user-overrides.js
+++ src/user-overrides.js
@@ -1,2 +1,8 @@
 // 5003: disable saving passwords
 user_pref("signon.rememberSignons", false);
+
+// 5010: disable location bar suggestion types
+user_pref("browser.urlbar.suggest.topsites", false);
+// user_pref("browser.urlbar.suggest.history", false);
+// user_pref("browser.urlbar.suggest.bookmark", false);
+// user_pref("browser.urlbar.suggest.openpage", false);
